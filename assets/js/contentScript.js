1664d2f45abaeec3a97f5f5c5ca650b7dd14a155
Parkhyeonshin <sshin4882@gmail.com>
2022-12-07 15:02:42 +0900

    22.120.7

--- assets/js/contentScript.js
+++ assets/js/contentScript.js
@@ -11,6 +11,41 @@ const constarray = [
     },
     {
         tit: `site list`,
+        subtit: `choice site`,
+        desc: `<span> 수강기간동안 만든 사이트들을 몇가지 추려서 모아놓은 INDEX홈페이지입니다. 각 카드들은 앞면은 3D뒤집기 효과, 뒷면은 일반 뒤집기 효과를 주어 구성해보았습니다. </span><span> SITE LIST에서는 그 중 몇가지를 모아봤습니다.</span>`,
+    },
+    {
+        tit: `site list`,
+        subtit: `site making`,
+        desc: `<span> 가장 처음으로 직접 만들어본 사이트입니다. 모든 기기에 유연하게 대응하는 반응형사이트에 초점을 맞춰 진행했습니다. 슬라이드는 Swiper 플러그인을 적용하여 만들어보았습니다.</span>`,
+    },
+    {
+        tit: `site list`,
+        subtit: `game effect`,
+        desc: `<span> 자바스크립트를 이용하여 제작한 게임모음 및 뮤직플레이어 사이트입니다. 뮤직플레이어, 테트리스게임 이외에도 그간 배웠던 효과들을 활용하여 메모리게임, 검색게임 들을 만들어 추가해 보았습니다. 모던레트로 컨셉으로 하되 각 게임은 레트로풍으로 기획해서 작업해보았습니다.</span>`,
+    },
+    {
+        tit: `site list`,
+        subtit: `React site`,
+        desc: `<span> REACT를 활용하여 만든 유튜브 사이트입니다. 유튜버, 구독자 수, 해시 태그, 최신 영상 등 Rapid사이트의 Youtube v3 API를 사용하여 데이터를 가져왔습니다.</span>`,
+    },
+    {
+        tit: `site list`,
+        subtit: `vue site`,
+        desc: `<span> REACT를 이용하여 만들어본 예제 사이트를 VUE로도 만들어 본 사이트입니다. REACT와 마찬가지로 unsplash, movie 등 레퍼런스데이터들은 api를 활용하여 가져와 작업했습니다. </span>`,
+    },
+    {
+        tit: `site list`,
+        subtit: `php site`,
+        desc: `<span> 팀 프로젝트로 진행하고, PHP를 활용하여 만든 사이트입니다. 실제 운용하는 사이트처럼 구현하는 것이 주요 목표였으며 목적에 부합한 로그인 기능, 댓글 기능, 글 작성/수정/삭제 기타 등등 다양한 기능들을 구현하여 만들었습니다.</span>`,
+    },
+    {
+        tit: `site list`,
+        subtit: `search game`,
+        desc: `<span> 자바스크립트를 이용하여 제작한 검색효과입니다. 특히, 이 검색효과를 이용하여 CSS속성 값 맞추기 게임을 만들어 보았습니다.</span>`,
+    },
+    {
+        tit: `contact`,
         subtit: `api site`,
         desc: `<span> 웹표준을 준수하며 모든 기기에 유연하게 대응하는 반응형사이트입니다. Swiper 플러그인을 적용하여 배너와 차트 탭, 이미지 슬라이드, 동영상 등 다양하면서도 편리한 기능을 담은 기업형 웹사이트로 작업했습니다.</span>`,
     },
