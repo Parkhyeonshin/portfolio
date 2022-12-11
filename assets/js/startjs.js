3a5cba43821420e508db99e3336e7c7c514b50b1
Parkhyeonshin <sshin4882@gmail.com>
2022-12-12 01:23:20 +0900

    22

--- assets/js/startjs.js
+++ assets/js/startjs.js
@@ -1,5 +1,6 @@
 const StartWindow = document.querySelector("#StartWindow");
 const start1 = document.querySelector(".start1");
+const start3 = document.querySelector(".start3");
 
 window.onload = function () {
     // var AudioContext;
@@ -21,6 +22,18 @@ window.onload = function () {
     setTimeout(() => {
         startfunc2();
     }, 5500);
+    setTimeout(() => {
+        startfunc3();
+    }, 9000);
+    setTimeout(() => {
+        startfunc3_2();
+    }, 15000);
+    setTimeout(() => {
+        startfunc3_3();
+    }, 15000 + 1075);
+    setTimeout(() => {
+        startfunc3_4();
+    }, 18075 + 3000);
 };
 
 function startfunc1() {
@@ -86,9 +99,53 @@ function startfunc1() {
     setTimeout(() => {
         StartWindow.querySelector(".start1").style.display = "none";
     }, StartWindow.querySelectorAll(".start1 > span").length * 225);
-    console.log(StartWindow.querySelectorAll(".start1 > span").length * 225);
 }
 
 function startfunc2() {
     StartWindow.querySelector(".start2").classList.add("active");
+    setTimeout(() => {
+        StartWindow.querySelector(".start2").style.display = "none";
+    }, 2500);
+}
+function startfunc3() {
+    start3.classList.add("active"); // 디플블락만
+    setTimeout(() => {
+        let introAudio = new Audio();
+        introAudio.src = "../audio/Laser1.mp3";
+        introAudio.volume = 0.6;
+        introAudio.play();
+    }, 4800);
+}
+
+function startfunc3_2() {
+    let splitText = `WEB DEV. PORTFOLIO`;
+    let splitWrap = splitText.split("").join('</span><span aria-hidden = "true">');
+    splitWrap = "<span aria-hidden = 'true'>" + splitWrap + "</span><div class = 'blinkText'></div>";
+    start3.querySelector(".start3__name").innerHTML = splitWrap;
+    document.querySelectorAll(".start3 > .start3__name > span").forEach((span, idx) => {
+        setTimeout(() => {
+            span.classList.add("active");
+        }, idx * 80);
+    });
+}
+function startfunc3_3() {
+    let splitText = `Made By. Hyeon Shin`;
+    let splitWrap = splitText.split("").join('</span><span aria-hidden = "true">');
+    splitWrap = "<span aria-hidden = 'true'>" + splitWrap + "</span><div class = 'blinkText'></div>";
+    start3.querySelector(".start3__desc").innerHTML = splitWrap;
+    document.querySelectorAll(".start3 > .start3__desc > span").forEach((span, idx) => {
+        setTimeout(() => {
+            span.classList.add("active");
+        }, idx * 80);
+    });
+    console.log(StartWindow.querySelectorAll(".start3 > .start3__desc > span").length * 225);
+}
+function startfunc3_4() {
+    start3.classList.add("passive");
+    setTimeout(() => {
+        let introAudio = new Audio();
+        introAudio.src = "../audio/Laser1.mp3";
+        introAudio.volume = 0.6;
+        introAudio.play();
+    }, 4800);
 }
